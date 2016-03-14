675f3876397c3fa50d5b1a3d757fcff4530d12b3
simplon-juliem <michelet.julie@yahoo.fr>
2016-03-14 17:28:31 +0100

    ajout informations.php

---
+++ landingpage1/assets/includes/informations.php
@@ -0,0 +1,61 @@
+<div class="infogene">
+
+  <?php include 'fondationsncf.php'; ?>
+
+  <div class="contenu">
+
+    <div class="titre-generale">
+      <h2>Informations générales</h2>
+      <a class="button" href="#"><button id="button" type="button" name="button">INSCRIPTION</button></a>
+    </div>
+
+    <div class="informations">
+
+      <div class="titre-objectif">
+        <img src="images/atome.png" alt="" />
+        <p>
+          L'objectif : Créer en 48 heures des solutions numériques au service de l'économie du partage.
+        </p>
+      </div>
+
+      <div class="titre-date">
+        <img src="images/calendrier.png" alt="" />
+        <p>
+          Dates : du 31 Mars 18h30 au 2 Avril 19h00
+        </p>
+      </div>
+
+      <div class="titre-lieu">
+        <img src="images/location.png" alt="" />
+        <p>
+          Lieu : IN'ESS - 30 Avenue Pompidor à Narbonne (Aude)
+        </p>
+      </div>
+
+      <div class="titre-candidat">
+        <img src="images/candidat.png" alt="" />
+        <p>
+          60 élèves  développeurs des écoles du numérique Simplon,<br>
+          20 étudiants des écoles du Web Design du Grand Sud,<br>
+          17 porteurs de projet sélectionnés ici,<br>
+        </p>
+
+        <iframe class="carte-g" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2909.3027641997537!2d2.9791705686777137!3d43.182157692999375!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12b1ac52defc6925%3A0xb10f76c5fd1d874a!2sIN&#39;ESS+Le+Grand+Narbonne!5e0!3m2!1sfr!2sfr!4v1457948346595" width="400px" height="250px"></iframe>
+
+        <p class="bloc-info">
+        <ul>
+          <li>✓ Initiés par <a class="info_ge_hyperlien" href="#">Simplon</a>, formation gratuite et intensive au numériques;<br></li>
+          <li>✓ Organisé par <a class="info_ge_hyperlien" href="#">IN'ESS</a>, la pépinière d'entreprise des projets de l'économie solidaire et sociale du Grand Narbonne;<br></li>
+          <li>✓ Porté par <a class="info_ge_hyperlien" href="#">Visionari</a>, spécialiste de l'innovation et des évenementiels entrepreneuriaux;</li>
+        </ul>
+        </p>
+      </div>
+
+      <p>
+        La participation à l’évènement Grand sud en partage est soumise à une présélection des projets par les organisateurs.<br>Chaque porteur de projet sera informé par mail le 29 mars au matin des résultats de la sélections.
+      </p>
+
+    </div>
+  </div>
+
+</div>
