b5b135f0586682ef4b4d0ab99e092008391eaed1
simplon-davidt <david@tarczewski.fr>
2016-03-14 12:29:04 +0100

    edition du titre de l'entete

--- landingpage1/assets/includes/logo-entete.php
+++ landingpage1/assets/includes/logo-entete.php
@@ -2,8 +2,11 @@
   <div class="contenu">
     <img src="../documents-de-reference/hgs.png" alt="hackathon grand sud logo" />
     <p>
-      1 er et 2 avril 2016 <br>
-      IN’ESS Narbonne
+      <span class="important-entete">HACKATHON</span> <br>
+      <span class="important-entete">Grand Sud en partage</span> <br>
+      Ensemble, inventons <br>
+      l’économie du partage au service de nos <br>
+      territoires
     </p>
   </div>
 </div>
