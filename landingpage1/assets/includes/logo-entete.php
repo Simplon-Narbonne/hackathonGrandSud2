9ce03585726664ce9170b288342173d3cef498e2
simplon-davidt <david@tarczewski.fr>
2016-03-14 12:16:44 +0100

    ajout de l'image de logo d'entete

--- landingpage1/assets/includes/logo-entete.php
+++ landingpage1/assets/includes/logo-entete.php
@@ -1,6 +1,6 @@
 <div class="logoentete" id="logoentete">
   <div class="contenu">
-    <img src="../documents-de-reference/logo.png" alt="hackathon grand sud logo" />
+    <img src="../documents-de-reference/hgs.png" alt="hackathon grand sud logo" />
     <p>
       1 er et 2 avril 2016 <br>
       IN’ESS Narbonne
