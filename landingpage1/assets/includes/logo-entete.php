c95ead831be52675bc94de36066e25a135287533
simplon-davidt <david@tarczewski.fr>
2016-03-15 11:07:53 +0100

    integration image du logo HGS

--- landingpage1/assets/includes/logo-entete.php
+++ landingpage1/assets/includes/logo-entete.php
@@ -1,15 +1,16 @@
 <div class="logoentete" id="logoentete">
 
-  <div class="contenu">
+    <div class="contenu">
 
-    <img src="../documents-de-reference/hgs.png" alt="hackathon grand sud logo" />
-    <p>
-      <span class="important-entete">HACKATHON</span> <br>
-      <span class="important-entete">Grand Sud en partage</span> <br>
-      Ensemble, inventons <br>
-      l’économie du partage au service de nos <br>
-      territoires
-    </p>
+      <img src="../documents-de-reference/img-landing-page.jpg" alt="hackathon grand sud logo" />
+
+      <p>
+        <span class="important-entete">HACKATHON</span> <br>
+        <span class="important-entete">Grand Sud en partage</span> <br>
+        Ensemble, inventons <br>
+        l’économie du partage au service de nos <br>
+        territoires
+      </p>
 
   </div>
 
