bde3a32cfb79a5a2b3563f883c73cbc3124b175c
simplon-davidt <david@tarczewski.fr>
2016-03-15 10:55:54 +0100

    Revert "mise a jour du logo et integration de la police ubuntu"
    This reverts commit 94529f99f524ad0c3b5edb8e00b3da21e54e8f20.

--- landingpage1/assets/includes/head.php
+++ landingpage1/assets/includes/head.php
@@ -4,7 +4,6 @@
     <meta charset="utf-8">
     <title>Hackathon Grand SUD</title>
     <link rel="stylesheet" href="assets/css/master.css">
-    <link href='https://fonts.googleapis.com/css?family=Ubuntu' rel='stylesheet' type='text/css'>
   </head>
   <body>
     <div class="mainpage">
--- landingpage1/assets/includes/logo-entete.php
+++ landingpage1/assets/includes/logo-entete.php
@@ -2,7 +2,7 @@
 
   <div class="contenu">
 
-    <img src="../documents-de-reference/img-landing-page.jpg" alt="hackathon grand sud logo" />
+    <img src="../documents-de-reference/hgs.png" alt="hackathon grand sud logo" />
     <p>
       <span class="important-entete">HACKATHON</span> <br>
       <span class="important-entete">Grand Sud en partage</span> <br>
